37339fa98a0d427e9795918e512a26efa985eeba
L.I <loulakis41@gmail.com>
2021-11-27 13:32:27 +0200

    error correction for queens=2 and add some System.out at main.java

--- N-Queens/src/Main.java
+++ N-Queens/src/Main.java
@@ -7,19 +7,20 @@ public class Main
     {
         Scanner scanner = new Scanner(System.in);
         System.out.println("\nGive the number of Queens : ");
-        int multitudeOfGenes = scanner.nextInt();
+        int numberOfQueens = scanner.nextInt();
         int minFitness = 0;
-        for (int i = 1; i <= multitudeOfGenes; i++){
+        for (int i = 1; i <= numberOfQueens; i++){
             // (n-1) + (n-2) + ... + (n-n)
-            minFitness += (multitudeOfGenes - i);
+            minFitness += (numberOfQueens - i);
         }
-        System.out.println("minFitness: " + minFitness);
+        System.out.println("- Chromosome's Fitness for the solution must be great or equal than " + minFitness + ".");
+        System.out.println("- The final chromosome depends on the number of repetitions of reproduction.");
+        System.out.println("- If chromosome's fitness is less than " + minFitness + ", the chromosome returned is the best of the last reproduction.");
         GeneticAlgorithm algorithm = new GeneticAlgorithm();
         long start = System.currentTimeMillis();
-        Chromosome solution = algorithm.run(multitudeOfGenes,1000, 0.05, 1000, minFitness);
+        Chromosome solution = algorithm.run(numberOfQueens,1000, 0.05, 500, minFitness);
         long end = System.currentTimeMillis();
         solution.print();
         System.out.println("\nSearch time: " + (double)(end - start) / 1000 + " seconds");
-        System.out.println(solution.getFitness());
     }
 }
